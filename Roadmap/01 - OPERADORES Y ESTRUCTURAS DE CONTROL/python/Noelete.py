11e7f39963d7da997eda6c8addcd0a35b0d0d3a3
Noelete <mestredescola@gmail.com>
2024-08-02 09:58:01 +0200

    Create Noelete.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/Noelete.py
@@ -0,0 +1,28 @@
+#https://python.org/
+
+#Comentario en una línea
+
+"""
+Estoy haciendo un comentario 
+en varias lineas
+"""
+
+ ´´´
+Así tambien
+se pueden añadir comentarios
+en varias lineas
+´´´
+
+
+mi_variable = "Mi variable"
+
+MY_CONSTANT = "mi constante" 
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_ string = "Mi cadena de texto"
+my_other_string = ´Mi otra cadena de texto´
+
+print("¡Hola, Python! Diosito, ayúdame")
